d45c7ada4821712b5f0c21b607d20c2678fa08df
craymont <craymontnicholls@strode-college.ac.uk>
2020-11-09 09:44:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def Checkheight(height):
+  if height >= 104:
+    print("You are tall enough to go on the ride")
+  else:
+    print("You are not tall enough to go on the ride.")
+
+
+Checkheight(103)
